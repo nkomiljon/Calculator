d33615bb34635fe1dc47408e872886c8c932ad7e
komiljon <komiljonnajmitdinov@gmail.com>
2020-10-26 19:43:31 +0500

    Calc

--- js/app.js
+++ js/app.js
@@ -1,87 +1,109 @@
-var wynik = 0,
-op = 0,
-nowe = 0,
-nowe2 = 0,
-done = 1,
-oset = 0,
-btn,
-temp;
-function reset(value) {
-document.form.window.value = value;
-wynik = 0,
-op = 0,
-nowe = 0,
-nowe2 = 0;
-done = 1;
-oset = 0;
-}
-function wspolna(new_temp) {
-btn = 1;
-if (nowe || done) {
-   nowe = 0;
-   done = 0;
-   temp = new_temp;
-} 
-for (var i = 0; i < temp.length; i++) {
-   if (temp[i] == '.') {
-       btn = 0;
-   }
-}
+/* 
+Этот JS файл не завершен .........
 
-}
-function button(ktroy, ktroy2) {
-temp = document.form.window.value;
-if (ktroy2 == '.') {
-   wspolna('0'); 
-if(btn) {
-   temp += ktroy2;
-   document.form.window.value = temp;
-   oset = 0;
-   }
-}
-if (ktroy >= 0  && ktroy <= 9) {
-  wspolna('');
-  if (temp == 0 && btn == 1) 
-      temp = '';
-      temp += ktroy;
-      document.form.window.value = temp;
-      oset = 1;
-  
-}
-  if (ktroy2 == '-' || ktroy2 == '+' || ktroy2 == '/' || ktroy2 == '*'){
-      if (nowe) 
-          op = ktroy2;
-      else {
-          if (!nowe2) {
-              op = ktroy2;
-              wynik = temp;
-              nowe2 = 1;
-          }
-       else {
-           wynik = eval(wynik + op + temp);
-           op = ktroy2;
-           document.form.window.value = wynik;
-       }
-       opset = 0;
-       nowe = 1;
-      }
-  }
-  if (ktroy2 == '1/x') {
-      wynik = eval(1 / temp); reset(wynik);
-  }
-  if (ktroy2 == 'sqrt') {
-      wynik = Math.sqrt(temp); reset(wynik);
-  }
-  if (ktroy2 == '+/-') {
-      document.form.window.value = eval(-temp);
-  }
-  if (ktroy2 == '=' && oset && op != '0') {
-      reset(eval(wynik + op + temp));
-  }
-  if (ktroy2 == 'C') { 
-      reset(0);
-  }
-  if (document.form.window.value[0] == '.') {
-      document.form.window.value = '0' + document.form.window.value;
-  }
-}
+*/
+(function () {
+    const display = document.getElementById('display');
+
+    const Calc = (str) => {
+        var arr = str.split(' ');
+
+        switch (arr[1]) {
+            case '+':
+                return Math.round((+arr[0] + +arr[2]) * 100) / 100;
+                break;
+            case '-':
+                return Math.round((+arr[0] - +arr[2]) * 100) / 100;
+                break;
+            case '/':
+                return Math.round((+arr[0] / +arr[2]) * 100) / 100;
+                break;
+            case '*':
+                return Math.round((+arr[0] * +arr[2]) * 100) / 100;
+                break;
+        };
+    };
+    var plusMinusButton = true;
+
+    const plusMinus = (str) => {
+        var arr = str.split(' ');
+        if (plusMinusButton && arr.length === 1) {
+            arr[0] = '-' + arr[0];
+            plusMinusButton = false;
+            return arr.join(' ');
+        } else if (!plusMinusButton && arr.length === 1) {
+          arr[0] = arr[0].replace(/[-]/g, "");
+          plusMinusButton = true;
+          return arr.join(' ');
+        };
+        if (plusMinusButton && arr.length > 2) {
+            arr[2] = '-' + arr[2];
+            plusMinusButton = false;
+            return arr.join(' ');
+        } else if (!plusMinusButton && arr.length > 2) {
+            arr[2] = arr[2].replace(/[-]/g, "");
+            plusMinusButton = true;
+            return arr.join(' ');
+        };
+    };
+    var booleanSing = true;
+    var count = 0;
+    var str = '';
+
+    const keyboard = document.getElementById('keyboard');
+    keyboard.addEventListener('click', (e) => {
+        var target = e.target;
+
+        if (target.value.match(/[0123456789\.]/)){
+            if (booleanSing === true) {
+                display.textContent = '';
+                booleanSing = false;
+            };
+            display.textContent += target.value;
+            str += target.value;
+        };
+
+       if (target.value.match(/[^0123456789\.=c%m]/)) {
+           count += 1;
+           if (count === 2) {
+               display.textContent = Calc(str);
+               str = Calc(str);
+               count = 1;
+           };
+           plusMinusButton = true;
+           str += ' ' + target.value + ' ';
+           booleanSing = true;
+       };
+
+       if (target.value.match('c')) {
+           count = 0;
+           booleanSing = true;
+           str = '';
+           display.textContent = '0';
+       };
+
+       if (target.value.match('%')) {
+           count = 0;
+           str = Calc(str) / 100;
+           display.textContent = str;
+           booleanSing = false;
+       };
+
+       if (target.value.match('=')) {
+           count = 0;
+           display.textContent = Calc(str);
+           str = Calc(str);
+           booleanSing = false;
+       };
+
+       if (target.value.match('m')) {
+           str = plusMinus(str);
+           var arr = str.split(' ');
+           if (arr.length > 2) {
+               display.textContent = arr[2];
+           } else {
+               display.textContent  = str;
+           };
+       };
+    });
+})();
